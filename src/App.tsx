d358b17753a5eb71ebb2a3b637308343297ebb95
jack <os-shenxj@jsfund.cn>
2022-07-04 15:38:59 +0800

    feat: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
+import CanvasGrid from "./pages/canvas-demo/grid";
 
 function App() {
   return (
     <div className="App">
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/canvas-basic">canvas基础知识</Link>|{" "}
-        <Link to="/canvas-box">canvas基础图形</Link>|{" "}
-        <Link to="/canvas-fabricjs">canvas FabricJs 工具库</Link>|{" "}
-        <Link to="/canvas-grid">canvas大图加载</Link>
-      </nav>
+      <CanvasGrid />
     </div>
   );
+  // return (
+  //   <div className="App">
+  //     <nav
+  //       style={{
+  //         borderBottom: "solid 1px",
+  //         paddingBottom: "1rem",
+  //       }}
+  //     >
+  //       <Link to="/canvas-basic">canvas基础知识</Link>|{" "}
+  //       <Link to="/canvas-box">canvas基础图形</Link>|{" "}
+  //       <Link to="/canvas-fabricjs">canvas FabricJs 工具库</Link>|{" "}
+  //       <Link to="/canvas-grid">canvas大图加载</Link>
+  //     </nav>
+  //   </div>
+  //);
 }
 
 export default App;
