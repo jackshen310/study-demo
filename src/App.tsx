b85ce7890f91c4b37406fb92cfb79546dbbc99ef
jack <os-shenxj@jsfund.cn>
2022-09-14 09:36:25 +0800

    feat(v-canvas): 简单接入

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ function App() {
         <Link to="/redux-counter">Redux-Counter</Link>|{" "}
         <Link to="/webgl-basic">Webgl-basic</Link>|{" "}
         <Link to="/three-basic">Three-basic</Link>|{" "}
-        <Link to="/rust">rust demo</Link>| <Link to="/wasm">wasm demo</Link>
+        <Link to="/rust">rust demo</Link>| <Link to="/wasm">wasm demo</Link>|{" "}
+        <Link to="/canvas-v">canvas v</Link>
       </nav>
     </div>
   );
