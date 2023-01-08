41339de173487457d373372a9221176d0ada651c
jack <os-shenxj@jsfund.cn>
2023-01-08 10:23:00 +0800

    feat(python): 初始化web项目

---
+++ src-py/learning_log/learning_logs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """用户学习的主题。"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """返回模型的字符串表示。"""
+        return self.text
